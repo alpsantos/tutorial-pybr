fa094600f54f94f748409823dd8779eaf0a65679
Alessandro Santos <alpsantos@gmail.com>
2023-05-01 20:06:51 -0300

    Adiciona documentação ao projeto

---
+++ api_pedidos/esquema.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    sku: str
+    description: str
+    image_url: str
+    reference: str
+    quantity: int
+
+class HealthCheckResponse(BaseModel):
+    status: str
+    
+class ErrorResponse(BaseModel):
+    message: str
